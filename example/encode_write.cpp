965b6674895f617eee793ff13d709e2df3b86511
Anders Lövgren <andlov@nowise.se>
2009-03-28 22:59:56 +0000

    No need for the extra newline (its added automatic by the encoder::write() methods.

--- example/encode_write.cpp
+++ example/encode_write.cpp
@@ -12,24 +12,24 @@ int main()
 	foa::encoder enc;
 	
 	enc.write("single line");
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	
 	enc.write("name", "anders");
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	enc.write("age", 39);
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	enc.write("bmi", 1.56);
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	
 	enc.write("object", foa::start_object);
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	enc.write(foa::end_object);
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 
 	enc.write("array", foa::start_array);
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	enc.write(foa::end_array);
-	std::cout << enc.buffer() << std::endl;
+	std::cout << enc.buffer();
 	
 	return 0;
 }
